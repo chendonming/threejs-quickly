d1809077da159bf3e44e9c2a13b7d19fa56fa31c
chendm <2283973011@qq.com>
2023-04-14 20:23:37 +0800

    feat: demo glsl

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,21 +1,26 @@
-<template>
-
-</template>
+<template></template>
 
 <script setup lang="ts">
 import * as THREE from "three";
+//@ts-ignore
+import test_vertex from "./glsl/test_vertex.glsl";
+//@ts-ignore
+import test_frag from "./glsl/test_frag.glsl";
 
 const props = defineProps({
   scene: THREE.Scene,
   renderer: THREE.WebGLRenderer,
   camera: THREE.PerspectiveCamera,
-  controls: Object
+  controls: Object,
 });
 const { scene, renderer, camera, controls } = props;
 if (scene && renderer && camera && controls) {
-  const geometry = new THREE.PlaneGeometry( 1, 1 );
-  const material = new THREE.MeshBasicMaterial( {color: 0xff0000, side: THREE.DoubleSide} );
-  const plane = new THREE.Mesh( geometry, material );
-  scene.add( plane );
+  const geometry = new THREE.PlaneGeometry(1, 1);
+  const material = new THREE.ShaderMaterial({
+    vertexShader: test_vertex,
+    fragmentShader: test_frag,
+  });
+  const plane = new THREE.Mesh(geometry, material);
+  scene.add(plane);
 }
 </script>
